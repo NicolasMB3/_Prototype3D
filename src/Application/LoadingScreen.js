ab0013e5607c872cf100b3e2037545951a838495
Nicolas <76515190+NicolasMB3@users.noreply.github.com>
2024-07-31 11:42:56 +0200

    Better loadingScreen :100:

--- src/Application/LoadingScreen.js
+++ src/Application/LoadingScreen.js
@@ -1,14 +1,14 @@
 import * as THREE from 'three';
 import Application from './Application.js';
 import gsap from 'gsap';
+import { CAMERA_SETTINGS } from "./variables.js";
 
 export default class LoadingScreen {
     constructor() {
         this.application = new Application();
-
         this.sizes = this.application.sizes;
-
         this.canvas = this.application.loadingCanvas;
+
         this.camera = new THREE.OrthographicCamera(
             this.sizes.width / -2,
             this.sizes.width / 2,
@@ -18,93 +18,109 @@ export default class LoadingScreen {
             10
         );
         this.camera.position.z = 1;
-        this.loadingScene = new THREE.Scene();
 
-        this.progressBar = document.querySelector('#loadingBar');
-        this.loadingItems = document.querySelector('#loadingItems');
+        this.container = document.querySelector('.container');
+        this.loadingScreenElement = document.getElementById('loadingScreen');
+        this.counterElement = document.getElementById('autoStartCounter');
+        this.currentDateTimeElement = document.getElementById('currentDateTime');
 
         this.setInstance();
-        this.init();
+        this.setTimeAndDate();
     }
 
     setInstance() {
         this.loadingManager = new THREE.LoadingManager(
-            () => {
-                this.loadComplete();
-            }, (url, itemsLoaded, itemsTotal) => {
-                this.inLoad(url, itemsLoaded, itemsTotal);
-            }
+            () => this.loadComplete(),
+            (url, itemsLoaded, itemsTotal) => this.inLoad(url, itemsLoaded, itemsTotal)
         );
-
-        // Uncomment only if the canvas is required for loading screen visuals
-        // this.renderer = new THREE.WebGLRenderer({ alpha: true });
-        // this.renderer.setSize(this.sizes.width, this.sizes.height);
-        // this.canvas.appendChild(this.renderer.domElement);
     }
 
     loadComplete() {
-        const cameraInstance = this.application.camera.instance;
-        const originalPosition = { x: 800, y: 3055, z: 2910 };
+        // Function to fade out the loading screen
+        const fadeOutLoadingScreen = () => {
+            gsap.to(this.loadingScreenElement, {
+                opacity: 0,
+                duration: 0.1,
+                onComplete: () => {
+                    this.loadingScreenElement.style.display = 'none';
+                }
+            });
+            this.container.style.display = 'block';
+            this.container.classList.add('fade-out');
+            animateCamera();
+        };
 
-        // Transition back to the original position
-        gsap.to(cameraInstance.position, {
-            duration: 1.2,
-            x: originalPosition.x,
-            y: originalPosition.y,
-            z: originalPosition.z,
-            ease: "power2.inOut"
-        });
+        // Function to animate the camera
+        const animateCamera = () => {
+            const cameraInstance = this.application.camera.instance;
+            const originalPosition = CAMERA_SETTINGS.positions[0];
+            gsap.to(cameraInstance.position, {
+                duration: 1.2,
+                x: originalPosition.x,
+                y: originalPosition.y,
+                z: originalPosition.z,
+                ease: "power2.inOut"
+            });
+        };
 
-        // Fade out the loading screen
-        gsap.to('#loadingScreen', {
-            opacity: 0,
-            duration: 2,
-            onComplete: () => {
-                document.getElementById('loadingScreen').style.display = 'none';
+        // Set a timeout to auto fade out the loading screen
+        const autoFadeOutTimeout = setTimeout(fadeOutLoadingScreen, 10000);
+
+        // Handle 'Enter' key press to manually fade out the loading screen
+        const onEnterPress = (event) => {
+            if (event.key === 'Enter') {
+                clearTimeout(autoFadeOutTimeout);
+                fadeOutLoadingScreen();
             }
-        });
+        };
+
+        window.addEventListener('keydown', onEnterPress);
+
+        // Start the countdown
+        this.startCountdown();
     }
 
     inLoad(url, itemsLoaded, itemsTotal) {
         const progress = itemsLoaded / itemsTotal;
-        this.progressBar.style.width = `${progress * 100}%`;
-        console.log(itemsLoaded / itemsTotal);
+        const fileIndex = itemsLoaded;
+        const currentFileElement = document.getElementById(`file${fileIndex}`);
+        const currentProgressElement = document.getElementById(`progress${fileIndex}`);
 
-        // Update loading items list
-        const itemName = url.split('/').pop();
-        this.loadingItems.innerHTML += `<li>${itemName} (${itemsLoaded}/${itemsTotal})</li>`;
+        if (currentFileElement) {
+            currentFileElement.textContent = `${url}`;
+        }
+
+        if (currentProgressElement) {
+            currentProgressElement.textContent = `: ${Math.floor((progress * 100) * 2.82)}%`;
+        }
+    }
+
+    startCountdown() {
+        let counter = 10;
+        const countdown = setInterval(() => {
+            counter -= 1;
+            this.counterElement.textContent = counter;
+            if (counter <= 0) {
+                clearInterval(countdown);
+            }
+        }, 1000);
     }
 
     getLoadingManager() {
         return this.loadingManager;
     }
 
-    init() {
-        this.geometry = new THREE.PlaneGeometry(this.sizes.width, this.sizes.height);
-        const material = new THREE.ShaderMaterial({
-            transparent: true,
-            uniforms: {
-                uAlpha: {
-                    value: 1,
-                }
-            },
-            vertexShader: `
-                void main() {
-                    gl_Position = vec4(position, 1.0);
-                }
-            `,
-            fragmentShader: `
-            uniform float uAlpha;
-            
-                void main() {
-                    gl_FragColor = vec4(0.0, 0.0, 0.0, uAlpha);
-                }
-            `
+    setTimeAndDate() {
+        const now = new Date();
+        const formattedDateTime = now.toLocaleString('fr-FR', {
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit',
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit'
         });
-        this.plane = new THREE.Mesh(this.geometry, material);
-
-        this.plane.position.set(0, 0, 0);
-        this.loadingScene.add(this.plane);
+        this.currentDateTimeElement.textContent = `:${formattedDateTime}`;
     }
 
     resize() {
@@ -113,14 +129,9 @@ export default class LoadingScreen {
         this.camera.top = this.sizes.height / 2;
         this.camera.bottom = this.sizes.height / -2;
         this.camera.updateProjectionMatrix();
-
-        this.plane.geometry.dispose();
-        this.plane.geometry = new THREE.PlaneGeometry(this.sizes.width, this.sizes.height);
-
-        // this.renderer.setSize(this.sizes.width, this.sizes.height);
     }
 
     update() {
-        // this.renderer.render(this.loadingScene, this.camera);
+        this.resize();
     }
 }
